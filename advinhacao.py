08fbb8305bea990117e6bb23a963bd8544d4bf13
Giovane Aguiar <giovane.aguiar@smartnx.com>
2022-10-17 16:29:53 -0300

    Implementa um jogo de advinhação.

--- advinhacao.py
+++ advinhacao.py
@@ -1,1 +1,14 @@
+print("********************************")
 print("Bem vindo ao jogo de Advinhação!")
+print("********************************")
+
+numero_secreto = 13
+
+chute_str = input("Digite o seu número: ")
+print("Você digitou ", chute_str)
+chute = int(chute_str)
+
+if numero_secreto == chute:
+    print("Você acertou o número secreto!")
+else:
+    print("Você errou o número secreto!")
